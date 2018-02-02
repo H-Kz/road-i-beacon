636cacc6210dbb5674ef1457b89a4e77a4695786
H-Kz <kazuya@net44-dhcp195.sfc.keio.ac.jp>
2018-02-03 07:37:57 +0900

    WEBサーバー機能作った。

--- beacon-central.js
+++ beacon-central.js
@@ -1,9 +1,34 @@
-Bleacon = require('bleacon');
+var Bleacon = require('bleacon');
+var express = require("express");
+var app = express();
 
-var uuid = '12345678-1234-1234-1234-123456789012';
+//マジックナンバー
+var PORT = 80
+
+//
+
+//静的ファイルで待ち受け
+app.use(express.static("html"))
+
+app.get("/start",function (req,res){
+    res.send("startしたで")
+})
+app.get("/stop",function (req,res){
+    res.send("stopしたで")
+})
+
+app.listen(80,()=>{
+    console.log("[EXPRESS] Webserver Listening. :" + PORT )
+})
+
+/*
+var uuid = '11451419198103643641145141919810';
 var major = 0;
 var minor = 0;
 var measuredPower = -59;
 
 Bleacon.startAdvertising(uuid, major, minor, measuredPower);
-Bleacon.stopAdvertising();
+
+var export_function = require('beacon-read.js');
+export_function.readUUID; 
+*/
