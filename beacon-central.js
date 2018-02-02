99cf493b1cfe1ac41cde2f6798174d41409a87d1
H-Kz <kazuya@net44-dhcp195.sfc.keio.ac.jp>
2018-02-03 07:43:29 +0900

    サーバー側OK

--- beacon-central.js
+++ beacon-central.js
@@ -5,30 +5,25 @@ var app = express();
 //マジックナンバー
 var PORT = 80
 
-//
-
 //静的ファイルで待ち受け
 app.use(express.static("html"))
-
+//startをGETで受け取る
 app.get("/start",function (req,res){
+    //uuidはqueryに入ってる。
+    var uuid = req.query.uuid;
+    var major = 0;
+    var minor = 0;
+    var measuredPower = -59;
+    Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+    console.log(`UUID:${uuid}`)
     res.send("startしたで")
 })
+//stopをGETで受け取る
 app.get("/stop",function (req,res){
+    Bleacon.stopAdvertising()
     res.send("stopしたで")
 })
 
 app.listen(80,()=>{
     console.log("[EXPRESS] Webserver Listening. :" + PORT )
 })
-
-/*
-var uuid = '11451419198103643641145141919810';
-var major = 0;
-var minor = 0;
-var measuredPower = -59;
-
-Bleacon.startAdvertising(uuid, major, minor, measuredPower);
-
-var export_function = require('beacon-read.js');
-export_function.readUUID; 
-*/
